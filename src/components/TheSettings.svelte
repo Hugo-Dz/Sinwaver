e36886a1b5837243ee0ee45cf7c5fdc64b073cbe
Hugo <duprez.hugo@gmail.com>
2022-03-19 15:34:05 +0100

    img

--- src/components/TheSettings.svelte
+++ src/components/TheSettings.svelte
@@ -17,7 +17,7 @@
 <div class="mainContainerSettings">
 
 
-  <Cursor cursorSetting="amplitudeValue" initialValue={storedValues.amplitudeValue} minValue={0} maxValue={450} minIconPath='../assets/amplitude icon min.svg' maxIconPath="/amplitude icon max.svg" />
+  <Cursor cursorSetting="amplitudeValue" initialValue={storedValues.amplitudeValue} minValue={0} maxValue={450} minIconPath='/assets/amplitude icon min.svg' maxIconPath="/amplitude icon max.svg" />
   <Cursor cursorSetting="lengthValue" initialValue={storedValues.lengthValue} minValue={7} maxValue={300} minIconPath="/length icon min.svg" maxIconPath="/length icon max.svg" />
   <Cursor cursorSetting="phaseValue" initialValue={storedValues.phaseValue} minValue={20} maxValue={1560} minIconPath="/start icon min.svg" maxIconPath="/start icon max.svg" />
   <Cursor cursorSetting="strokeValue" initialValue={storedValues.strokeValue} minValue={1} maxValue={40} minIconPath="/stroke icon min.svg" maxIconPath="/stroke icon max.svg" />
